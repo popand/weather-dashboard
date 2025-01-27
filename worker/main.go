a8e7ed3be6ee55a32079a1ad37f0acce98f88a2f
Andrei Pop (anpop) <anpop@cisco.com>
2025-01-27 10:02:31 -0500

    Configure Temporal client using Viper for dynamic host port configuration

--- worker/main.go
+++ worker/main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"log"
-
 	"weather-dashboard/weather"
 
+	"github.com/spf13/viper"
 	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
 )
 
 func main() {
+	// Initialize viper
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("config")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalln("Error reading config:", err)
+	}
+
 	c, err := client.Dial(client.Options{
-		HostPort: "viaduct.proxy.rlwy.net:46280",
+		HostPort: viper.GetString("temporal.host_port"),
 	})
 	if err != nil {
 		log.Fatalln("Unable to create Temporal client:", err)
